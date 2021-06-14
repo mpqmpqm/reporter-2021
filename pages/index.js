7173dee53d3395167e4c2f7bf0711ea5aaaa6511
MPQ <mpq@mpq.dev>
2021-06-14 14:52:53 -0400

    fix dynamic meta tag

--- pages/index.js
+++ pages/index.js
@@ -34,7 +34,10 @@ const App = ({ onboarded, hostname }) => {
           rel="icon"
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>😘</text></svg>"
         />
-        <meta name="og:image" content={`${hostname}/reporter-meta.jpg`} />
+        <meta
+          name="og:image"
+          content={`https://${hostname}/reporter-meta.jpg`}
+        />
       </Head>
       <div className="App">
         {user && selectedBoard ? (
