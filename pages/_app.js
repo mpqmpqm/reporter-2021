912aad54bc1a45ac51533028051603e71a296c14
MPQ <mpq@mpq.dev>
2021-06-14 16:13:03 -0400

    update catchphrase

--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,7 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <title>Reporter</title>
-        <meta name="description" content="Reporter" />
+        <meta name="description" content="A protean quantified self app" />
         <link
           rel="icon"
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>😘</text></svg>"
@@ -16,7 +16,7 @@ function MyApp({ Component, pageProps }) {
           name="og:image"
           content={`https://${pageProps.hostname}/reporter-meta.jpg`}
         />
-        <meta name="og:description" content="Reporter" />
+        <meta name="og:description" content="A protean quantified self app" />
         <meta name="og:site_name" content="Reporter" />
         <meta name="twitter:card" content="summary_large_image"></meta>
         <meta name="og:url" content={`https://${pageProps.hostname}`} />
